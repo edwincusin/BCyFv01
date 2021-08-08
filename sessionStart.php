fb2f62994b5a7f0443cb47164dc125a16c44b883
edwinecuador <edwin@ecuador.com>
2021-08-08 18:21:54 -0500

    clonar pc EFGR

--- sessionStart.php
+++ sessionStart.php
@@ -5,7 +5,7 @@
     $varsession=$_SESSION['nameuser'];
     if($varsession==null || $varsession==" "){
         echo'<script type="text/javascript">
-                alert("Usuario sin identificarse, Ud. no tiene autorizacion para navegar en el aplicativo SICCOPES, soria a la camara");
+                alert("Usuario sin identificarse, Ud. no tiene autorizacion para navegar en el aplicativo Banco Sigue Adelante, sonria a la camara");
                 window.location.href="index.php";            
                 </script>';
              die();
